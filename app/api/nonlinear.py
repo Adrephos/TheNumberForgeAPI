b33dc6144bdf2a59c05f05f47f74f0a4a60ca3d8
Adrephos <adrephos@gmail.com>
2024-05-23 11:11:39 -0500

    chore: add error catching to nonlinear endpoints

--- app/api/nonlinear.py
+++ app/api/nonlinear.py
@@ -24,53 +24,71 @@ def resolve_response(result, table, error, response: Response):
 
 @router.post("/bisection")
 def bisection(input_data: NonlinearModels.Bisection, response: Response):
-    result, table, error = NonlinearService.Bisection(
-        input_data.a, input_data.b, input_data.fx,
-        input_data.tol, input_data.niter, input_data.relativeError
-    )
+    try:
+        result, table, error = NonlinearService.Bisection(
+            input_data.a, input_data.b, input_data.fx,
+            input_data.tol, input_data.niter, input_data.relativeError
+        )
+    except Exception:
+        return ResponseModel(None, False, "Invalid input")
     return resolve_response(result, table, error, response)
 
 
 @router.post("/fixed_point")
 def fixed_point(input_data: NonlinearModels.FixedPoint, response: Response):
-    result, table, error = NonlinearService.Fixed_point(
-        input_data.x0, input_data.fx, input_data.gx,
-        input_data.tol, input_data.niter, input_data.relativeError
-    )
+    try:
+        result, table, error = NonlinearService.Fixed_point(
+            input_data.x0, input_data.fx, input_data.gx,
+            input_data.tol, input_data.niter, input_data.relativeError
+        )
+    except Exception:
+        return ResponseModel(None, False, "Invalid input")
     return resolve_response(result, table, error, response)
 
 
 @router.post("/false_position")
 def false_position(input_data: NonlinearModels.FalsePosition, response: Response):
-    result, table, error = NonlinearService.False_position(
-        input_data.a, input_data.b, input_data.fx,
-        input_data.tol, input_data.niter, input_data.relativeError
-    )
+    try:
+        result, table, error = NonlinearService.False_position(
+            input_data.a, input_data.b, input_data.fx,
+            input_data.tol, input_data.niter, input_data.relativeError
+        )
+    except Exception:
+        return ResponseModel(None, False, "Invalid input")
     return resolve_response(result, table, error, response)
 
 
 @router.post("/newton")
 def newton(input_data: NonlinearModels.Newton, response: Response):
-    result, table, error = NonlinearService.Newton(
-        input_data.x0, input_data.fx, input_data.tol,
-        input_data.niter, False, input_data.relativeError
-    )
+    try:
+        result, table, error = NonlinearService.Newton(
+            input_data.x0, input_data.fx, input_data.tol,
+            input_data.niter, False, input_data.relativeError
+        )
+    except Exception:
+        return ResponseModel(None, False, "Invalid input")
     return resolve_response(result, table, error, response)
 
 
 @router.post("/secant")
 def secant(input_data: NonlinearModels.Secant, response: Response):
-    result, table, error = NonlinearService.Secant(
-        input_data.x0, input_data.x1, input_data.fx,
-        input_data.tol, input_data.niter, input_data.relativeError
-    )
+    try:
+        result, table, error = NonlinearService.Secant(
+            input_data.x0, input_data.x1, input_data.fx,
+            input_data.tol, input_data.niter, input_data.relativeError
+        )
+    except Exception:
+        return ResponseModel(None, False, "Invalid input")
     return resolve_response(result, table, error, response)
 
 
 @router.post("/multiple_roots")
 def multiple_roots(input_data: NonlinearModels.Newton, response: Response):
-    result, table, error = NonlinearService.Newton(
-        input_data.x0, input_data.fx, input_data.tol,
-        input_data.niter, True, input_data.relativeError
-    )
+    try:
+        result, table, error = NonlinearService.Newton(
+            input_data.x0, input_data.fx, input_data.tol,
+            input_data.niter, True, input_data.relativeError
+        )
+    except Exception:
+        return ResponseModel(None, False, "Invalid input")
     return resolve_response(result, table, error, response)
